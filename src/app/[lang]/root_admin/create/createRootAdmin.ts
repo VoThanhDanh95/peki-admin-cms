0a0a858abfd6955269bb9f7b6442b81252764bab
hacaohao95 <hacaohao95@gmail.com>
2024-04-08 21:44:10 +0700

    hash password

--- src/app/[lang]/root_admin/create/createRootAdmin.ts
+++ src/app/[lang]/root_admin/create/createRootAdmin.ts
@@ -3,6 +3,7 @@
 import { z } from "zod"
 import * as values from "./components/values"
 import prisma from "@/db"
+import bcrypt from 'bcryptjs'
 
 const createRootAdminSchema = z.object({
   userName: z.string().min(1),
@@ -11,11 +12,12 @@ const createRootAdminSchema = z.object({
 
 const createRootAdmin = async (data: values.FormType) => {
   const validData = await createRootAdminSchema.parseAsync(data)
+  const hashedPassword = await bcrypt.hash(validData.password, 10)    
 
   await prisma.user.create({
     data: {
       userName: validData.userName,
-      password: validData.password,
+      password: hashedPassword,
       role: {
         connect: {
           name: "ROOT_ADMIN",
