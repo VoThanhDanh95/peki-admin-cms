2e7ba0ef9c50515433c28e05ff6ffbea30386044
hacaohao95 <hacaohao95@gmail.com>
2024-05-26 16:51:53 +0700

    add color to label of exercise's fields

--- src/pages/exercise/ExerciseDetail.tsx
+++ src/pages/exercise/ExerciseDetail.tsx
@@ -1,8 +1,8 @@
-import { DateField, DeleteButton, EditButton, FunctionField, NumberField, ReferenceManyField, Show, SimpleShowLayout, TabbedShowLayout, TextField, TopToolbar } from "react-admin";
+import { Grid } from '@mui/material';
+import { DateField, DeleteButton, EditButton, FunctionField, Labeled, NumberField, ReferenceManyField, Show, SimpleShowLayout, TabbedShowLayout, TextField, TopToolbar } from "react-admin";
 import { SimpleExercise } from "../../types/exercise";
 import QuestionContentDataGrid from "../shared/QuestionContentDatagrid";
 import ExerciseAvailabilityField from "./components/ExerciseAvailabilityField";
-import { Grid } from '@mui/material';
 
 const ExerciseDetailShowActions = () => {
     return (
@@ -21,49 +21,70 @@ const ExerciseDetail = () => {
                     <Grid container spacing={2}>
                         <Grid item xs={6}>
                             <SimpleShowLayout>
-                                <TextField source="name" />
-                                <TextField source="intro" />
-                                <TextField source="category" />
+                                <Labeled color="primary.main">
+                                    <TextField source="name" />
+                                </Labeled>
+                                <Labeled color="primary.main">
+                                    <TextField source="intro" />
+                                </Labeled>
+                                <Labeled color="primary.main">
+                                    <TextField source="category" />
+                                </Labeled>
                             </SimpleShowLayout>
                         </Grid>
                         <Grid item xs={6}>
                             <Grid container>
                                 <Grid item xs={4}>
                                     <SimpleShowLayout>
-                                        <TextField source="mode" />
-                                        <FunctionField<SimpleExercise>
-                                            label="Duration"
-                                            sortBy='duration'
-                                            sortable
-                                            render={(r) => `${r.duration / 60} mins`}
-                                        />
+                                        <Labeled color="primary.main">
+                                            <TextField source="mode" />
+                                        </Labeled>
+                                        <Labeled color="primary.main">
+                                            <FunctionField<SimpleExercise>
+                                                label="Duration"
+                                                sortBy='duration'
+                                                sortable
+                                                render={(r) => `${r.duration / 60} mins`}
+                                            />
+                                        </Labeled>
                                     </SimpleShowLayout>
                                 </Grid>
                                 <Grid item xs={4}>
                                     <SimpleShowLayout>
-                                        <NumberField source="level" />
-                                        <FunctionField<SimpleExercise>
-                                            label="Availability"
-                                            sortBy='startAt'
-                                            sortable
-                                            render={(r) => <ExerciseAvailabilityField startAt={r.startAt} endAt={r.endAt} />}
-                                        />
+                                        <Labeled color="primary.main">
+                                            <NumberField source="level" />
+                                        </Labeled>
+                                        <Labeled color="primary.main">
+                                            <FunctionField<SimpleExercise>
+                                                label="Availability"
+                                                sortBy='startAt'
+                                                sortable
+                                                render={(r) => <ExerciseAvailabilityField startAt={r.startAt} endAt={r.endAt} />}
+                                            />
+                                        </Labeled>
                                     </SimpleShowLayout>
-
                                 </Grid>
                                 <Grid item xs={4}>
                                     <SimpleShowLayout>
-                                        <TextField source="skill" />
-                                        <TextField source="status" />
+                                        <Labeled color="primary.main">
+                                            <TextField source="skill" />
+                                        </Labeled>
+                                        <Labeled color="primary.main">
+                                            <TextField source="status" />
+                                        </Labeled>
                                     </SimpleShowLayout>
-
                                 </Grid>
                             </Grid>
 
                         </Grid>
                     </Grid>
-                    <TextField source="id" />
-                    <DateField source="createAt" />
+                    <Labeled color="primary.main">
+                        <TextField source="id" />
+                    </Labeled>
+                    <Labeled color="primary.main">
+                        <DateField source="createAt" />
+                    </Labeled>
+
                 </TabbedShowLayout.Tab>
                 <TabbedShowLayout.Tab label={"Question Content"}>
                     <ReferenceManyField
